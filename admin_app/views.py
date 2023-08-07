2c7f99ce702f0cf894ac8b2a5f99003e0f474597
Saber <sabersouhaimi.cs@gmail.com>
2023-08-07 15:30:12 +0200

    data = None changed

--- admin_app/views.py
+++ admin_app/views.py
@@ -825,11 +825,15 @@ def search_method_result(request,result):
         data = None  # Initialize data to None
         if method == "email":
             data = Order.objects.filter(email__contains=result[0])
+            return render(request, 'search_result.html', {"data": data})
+
         elif method == "pn":
             data = Order.objects.filter(ntel=result[0])
+            return render(request, 'search_result.html', {"data": data})
+
         elif method == "nom":
             data = Order.objects.filter(nom__contains=result[0])
-        return render(request, 'search_result.html', {"data": data})
+            return render(request, 'search_result.html', {"data": data})
     else:
         return redirect("login_user")
 def add_remarque(request,ordre_id):
